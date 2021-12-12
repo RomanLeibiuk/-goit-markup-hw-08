e3405df0243350007f2bc1733d5d4dcfad8dada2
RomanLeibiuk <92354812+RomanLeibiuk@users.noreply.github.com>
2021-12-12 20:34:49 +0200

    tyyu

--- js/header-btn.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const mobileMenu = document.querySelector('[data-menu-button]');
-
-  menuBtnRef .addEventListenter("click",()=>{
-    mobileMenu.classList.toggle('is_open');
-  });
-})();
